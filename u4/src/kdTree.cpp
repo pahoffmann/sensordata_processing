b537bf0c05416905662ce25fd024f549aa83b75c
sebzielinski <szielinski@uni-osnabrueck.de>
2021-06-10 16:00:51 +0200

    added kdTree class template

---
+++ u4/src/kdTree.cpp
@@ -0,0 +1,17 @@
+#include "kdTree.h"
+
+KDtree::KDtree(double **pts, int n) {
+
+}
+
+KDtree::~KDtree() {
+
+}
+
+double *KDtree::FindClosest(double *_p, double maxdist2) {
+
+}
+
+void KDtree::_FindClosest() {
+    
+}
---
+++ u4/src/kdTree.h
@@ -0,0 +1,29 @@
+#ifndef KDTREE_H
+#define KDTREE_H
+
+class KDtree {
+    public:
+        int pnts;       // 0 for inner nodes, others for leaves
+        union {
+            struct {
+                double center[3];
+                double dx, dy, dz, r2;
+                int splitaxis;
+                KDtree *child1, *child2;
+            } node;
+            struct {
+                double *p[8];
+            } leaf;
+        } ;
+        KDtree (double **pts, int n);
+        ~KDtree ();
+        double *FindClosest (double *_p, double maxdist2);
+    private:
+        static double *closest;
+        static double closest_d2;
+        static double *p;
+        void _FindClosest();
+
+};
+
+#endif
